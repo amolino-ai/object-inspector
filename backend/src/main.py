e09deb0007a4820465d52f309a5521a68dd666ac
Avni Rao <avnir@uw.edu>
2024-07-02 12:52:25 -0700

    main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,11 +2,44 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+mock_items = [
+    {"id": 1, "name": "Item 1", "value": 5},
+    {"id": 2, "name": "Item 2", "value": 121},
+    {"id": 3, "name": "Item 3", "value": 4},
+]
+
+mock_users = [
+    {"id": 1, "username": "user1", "email": "user1@example.com"},
+    {"id": 2, "username": "user2", "email": "user2@example.com"},
+    {"id": 3, "username": "user3", "email": "user3@example.com"},
+]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "hey"}
 
-@app.get("/avni")
-async def avni():
-    return {"message": "hi avni!"}
+@app.get("/items")
+async def get_items():
+    return mock_items
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int):
+    item = None
+    for i in mock_items:
+        if i["id"] == item_id:
+            item = i; 
+            break
+    return item
+
+@app.get("/users")
+async def get_users():
+    return mock_users
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    user = None
+    for u in mock_users:
+        if u["id"] == user_id:
+            user = u
+            break
+    return user
